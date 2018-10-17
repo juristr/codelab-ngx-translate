43ce5891922ccfd1093c3b529b8d8818e8b3dcdc
Juri <juri.strumpflohner@gmail.com>
2018-10-17 20:52:50 +0200

    install and configure ngx-translate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, SharedModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    SharedModule,
+    TranslateModule.forRoot()
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
